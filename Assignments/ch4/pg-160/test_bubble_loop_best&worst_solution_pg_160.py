f178c198b4789838b3132dad38fc06df8b2de29f
mainurrasel <mainurrrasel@gmail.com>
2020-12-28 15:27:09 +0600

    Assignments added upto 178 page

--- Assignments/ch4/pg-160/test_bubble_loop_best&worst_solution_pg_160.py
+++ Assignments/ch4/pg-160/test_bubble_loop_best&worst_solution_pg_160.py
@@ -1,9 +1,9 @@
 scores = [60, 50, 60, 58, 54, 54,
-            58, 50, 52, 54, 48, 69,
-            34, 55, 51, 52, 44, 51,
-            69, 64, 66, 55, 52, 61,
-            46, 31, 57, 52, 44, 18,
-            41, 53, 55, 61, 51, 44]
+58, 50, 52, 54, 48, 69,
+34, 55, 51, 52, 44, 51,
+69, 64, 66, 55, 52, 61,
+46, 31, 57, 52, 44, 18,
+41, 53, 55, 61, 51, 44]
 high_score = 0
 length = len(scores)
 #computing scores with index number
@@ -22,6 +22,7 @@ for i in range(length):
     if high_score == scores[i]:
         best_solutions.append(i)
 print('Solutions with the highest score:', best_solutions)
+
 #computing lowest score
 lowest_score = 100
 for i in range(length):
@@ -35,4 +36,3 @@ for i in range(length):
     if lowest_score == scores[i]:
         worst_solutions.append(i)
 print('Solutions with the lowest score:', worst_solutions)
-
